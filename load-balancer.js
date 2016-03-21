cadc8a9e9e5557a0beae8a30245f3b43f9018c06
Zachary Lester <zachary.greg.lester@gmail.com>
2016-03-21 11:47:18 -0700

    Modify load-balancer

--- load-balancer.js
+++ load-balancer.js
@@ -1,8 +1,8 @@
 var arguments = process.argv.splice(2);
-// var http = require('http');
 var express = require('express')
 var httpProxy = require('http-proxy');
 var proxy = httpProxy.createProxyServer({});
+var app = express();
 var port = arguments[0] || 3000;
 
 var addresses = [
@@ -18,15 +18,11 @@ var addresses = [
 ];
 
 var i = 0;
-var app = express();
+
 app.get('/', function(req, res) {
   proxy.proxyRequest(req, res, addresses[i]);
   i = (i + 1) % addresses.length;
 });
-// var server = http.createServer(function (req, res) {
-  // proxy.proxyRequest(req, res, addresses[i]);
-  // i = (i + 1) % addresses.length;
-// })
 
 app.listen(port, function() {
   console.log('Proxy server listening on ' + port);
