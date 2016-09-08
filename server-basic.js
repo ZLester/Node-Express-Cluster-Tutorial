c18fc3dbfcf90caaae207763c59e27acc43b343f
Zachary Lester <zachary.greg.lester@gmail.com>
2016-09-08 12:25:06 -0700

    Update example code

--- server-basic.js
+++ server-basic.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var app = express();
-var args = process.argv.splice(2);
-var port = args[0] || 3000;
+var port = 3000;
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   console.log('Request to server on ' + port);
   for (var i = 0; i < 20000000; i++) {
-    // do some unneccessary work
+    // Do some unneccessary work so subsequent requests slow down.
   }
   res.send('Q: Why are JS devs so bad at therapy? A: They just don\'t Node how to Express themelves.');
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Server listening on port ' + port);
 });
--- server-cluster.js
+++ server-cluster.js
@@ -1,34 +1,39 @@
 var cluster = require('cluster');
-var args = process.argv.splice(2);
 
 if (cluster.isMaster) {
+  // Use the OS module to check how many core's the current machine has.
   var cpus = require('os').cpus();
-  cpus.forEach(function(cpu) {
+
+  // For each core, create a new worker
+  cpus.forEach(function (cpu) {
     cluster.fork();
   });
 
-  cluster.on('online', function(worker) {
+  // Workers will emit an 'online' event when they spawn
+  cluster.on('online', function (worker) {
     console.log('Worker ' + worker.id + ' is here to chew bubblegum and scale node applications.');
   });
 
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('Worker ' + worker.id + ' died. RIP in peace.');
+  // Workers will emit an 'exit' event when they exit
+  cluster.on('exit', function (worker, code, signal) {
+    console.log('Worker ' + worker.id + ' died with code ' + code + '. RIP in peace.');
+    // You can maintain a constant number of workers by forking when a worker exits
     cluster.fork();
   });
 } else {
   var express = require('express');
   var app = express();
-  var port = args[0] || 3000;
+  var port = 3000;
 
-  app.get('/', function(req, res) {
-    console.log('Request to worker ' + cluster.worker.id); 
+  app.get('/', function (req, res) {
+    console.log('Request to worker ' + cluster.worker.id);
     for (var i = 0; i < 20000000; i++) {
-      // do some unneccessary work
+      // Do some unneccessary work so subsequent requests slow down.
     }
     res.send('Q: Why are JS devs so bad at therapy? A: They just don\'t Node how to Express themelves.');
   });
 
-  app.listen(port, function() {
+  app.listen(port, function () {
     console.log('Worker ' + cluster.worker.id + ' is all out of bubblegum on port ' + port);
   });
 }
